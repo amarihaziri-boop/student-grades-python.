54d3d5dd3608ad0e46c6627ea58a29637db6608d
amarihaziri-boop <amarihaziri@gmail.com>
2025-12-25 18:05:39 +0100

    Add files via upload

---
+++ projectMe.py
@@ -0,0 +1,20 @@
+# Lista me nota studentësh
+grades = [78, 85, 90, 67, 92, 88]
+
+# Llogarit statistikat
+average = sum(grades) / len(grades)
+maximum = max(grades)
+minimum = min(grades)
+
+# Printon rezultatet me 2 shifra pas presjes
+print("Nota mesatare:", round(average, 2))
+print("Nota më e lartë:", maximum)
+print("Nota më e ulët:", minimum)
+
+
+
+
+
+
+
+
